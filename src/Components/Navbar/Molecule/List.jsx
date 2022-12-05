8bcf09b400d84ea5d9d11a846b5b2ae2e7ed1e5a
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-05 19:27:47 +0800

    change: navbar list

--- src/Components/Navbar/Molecule/List.jsx
+++ src/Components/Navbar/Molecule/List.jsx
@@ -8,12 +8,12 @@ export const List = ({open}) => {
             <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>About</a></p>
         </div>
         <div className='text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background'>
-            <i className="fa-solid fa-comments text-secondary"></i>
-            <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>Experience</a></p>
+            <i class="fa-solid fa-people-group text-secondary"></i>
+            <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>Team</a></p>
         </div>
         <div className='text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background'>
             <i className="fa-sharp fa-solid fa-briefcase text-secondary"></i>
-            <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>Work</a></p>
+            <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>Skills</a></p>
         </div>
         <div className='text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background'>
             <i className="fa-solid fa-address-card text-secondary"></i>
