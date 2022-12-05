c0460371fa2c3b245924dffc909bf2407d858bf0
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-05 11:19:06 +0800

    change: style

--- src/Components/Navbar/Molecule/List.jsx
+++ src/Components/Navbar/Molecule/List.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 export const List = ({open}) => {
   return (
     <div className='flex justify-center items-center h-[70vh] flex-col'>
-        <div className='text-center text-lg my-4'>
+        <div className='text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background'>
             <i className="fa-sharp fa-solid fa-exclamation text-secondary"></i>
             <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>About</a></p>
         </div>
-        <div className='text-center text-lg my-4'>
+        <div className='text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background'>
             <i className="fa-solid fa-comments text-secondary"></i>
             <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>Experience</a></p>
         </div>
-        <div className='text-center text-lg my-4'>
+        <div className='text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background'>
             <i className="fa-sharp fa-solid fa-briefcase text-secondary"></i>
             <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>Work</a></p>
         </div>
-        <div className='text-center text-lg my-4'>
+        <div className='text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background'>
             <i className="fa-solid fa-address-card text-secondary"></i>
             <p><a href="" className={`text-primary ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-300 `}>Contact</a></p>
         </div>
