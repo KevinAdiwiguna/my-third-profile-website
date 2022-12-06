f25c9af6fdfe6a8491331018930704d487498896
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-06 21:27:49 +0800

    change: navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,36 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 // Components
 import { List } from './Molecule/List'
 export const Navbar = () => {
   const [open, setOpen] = useState(false)
-   
+  const [scroll, setScroll] = useState(false)
+
+  const navbarAppear = () => {
+    if (window.scrollY > 250) {
+      setScroll(true)
+    } else {
+      setScroll(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', navbarAppear)
+    return () => {
+      window.removeEventListener('scroll', navbarAppear)
+    }
+  })
+
+
   return (
-    <div className={`${open ? "w-80" : "w-0"} h-screen bg-slidebar duration-300 fixed z-10`}>
-        <div className=''>
-            <div className={`flex text-primary items-center p-8`} onClick={() => setOpen(!open)}>
-                <i className={`fa-solid fa-bars text-secondary ${open ? "rotate-90" : "rotate-0"} duration-300`}></i>
-                <p className={`px-3 text-lg ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-200`}>VinEnd</p>
-            </div>
-            <List open={open}/>
+    <div className={` ${open ? "!w-80" : "w-20"}  ${scroll ? "w-0" : "w-20"}    h-screen bg-slidebar duration-300 fixed open`
+    }>
+      <div className=''>
+        <div className={`flex text-primary items-center p-8`} onClick={() => setOpen(!open)}>
+          <i className={`fa-solid fa-bars ${open ? "rotate-0" : "rotate-90"} duration-300`}></i>
+          <p className={`px-3 text-lg ${open ? "opacity-100" : "opacity-0 pointer-events-none"} duration-200`}>VinEnd</p>
         </div>
+        <List open={open} scroll={scroll}/>
+      </div>
     </div>
   )
 }
