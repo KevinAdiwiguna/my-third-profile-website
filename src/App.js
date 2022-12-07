96853d5c847abc602996fb481ea0a461efd1d2b1
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-07 21:54:47 +0800

    nothing changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Hero } from "./Components/Hero/Hero";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Team } from "./Components/Team/Team";
-
+import { About } from "./Components/About/About"
 function App() {
   return (
     <div>
       <Navbar />
       <Hero/>
+      <About />
       <Team />
     </div>
   );
