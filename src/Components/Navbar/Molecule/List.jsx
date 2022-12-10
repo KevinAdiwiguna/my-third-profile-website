12fe5cf4b172cf0941bf45417223d28949f76975
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-10 22:13:50 +0800

    fixed: navbar event

--- src/Components/Navbar/Molecule/List.jsx
+++ src/Components/Navbar/Molecule/List.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 export const List = (props) => {
   return (
     <div className='flex justify-center items-center h-[70vh] flex-col'>
-        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100' : "opacity-0"} duration-300`}>
+        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100 pointer-events-auto' : "opacity-0"} duration-300`}>
             <i className="fa-sharp fa-solid fa-exclamation text-secondary"></i>
             <p><a href="" className={`text-primary ${props.open ? "sm:!opacity-100" : "sm:opacity-0 pointer-events-none"} duration-300 `}>About</a></p>
         </div>
-        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100' : "opacity-0"} duration-300`}>
+        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100 pointer-events-auto' : "opacity-0"} duration-300`}>
             <i className="fa-solid fa-people-group text-secondary"></i>
             <p><a href="" className={`text-primary ${props.open ? "sm:!opacity-100" : "sm:opacity-0 pointer-events-none"} duration-300 `}>Team</a></p>
         </div>
-        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100' : "opacity-0"} duration-300`}>
+        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100 pointer-events-auto' : "opacity-0"} duration-300`}>
             <i className="fa-sharp fa-solid fa-briefcase text-secondary"></i>
             <p><a href="" className={`text-primary ${props.open ? "sm:!opacity-100" : "sm:opacity-0 pointer-events-none"} duration-300 `}>Skills</a></p>
         </div>
-        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100' : "opacity-0"} duration-300`}>
+        <div className={`text-center text-lg px-5 pb-4 pt-10 rounded-lg hover:bg-background ${props.scroll ? "sm:opacity-0 pointer-events-none" : "sm:opacity-100 sm:pointer-events-auto"} ${props.open ? 'sm:!opacity-100' : ""}  ${props.open ? 'opacity-100 pointer-events-auto' : "opacity-0"} duration-300`}>
             <i className="fa-solid fa-address-card text-secondary"></i>
             <p><a href="" className={`text-primary ${props.open ? "!opacity-100" : "sm:opacity-0 pointer-events-none"} duration-300 `}>Contact</a></p>
         </div>
