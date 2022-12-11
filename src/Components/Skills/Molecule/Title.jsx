03ef936189f502fce7e99c8a3781589840835430
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-11 12:36:18 +0800

    change: font color

--- src/Components/Skills/Molecule/Title.jsx
+++ src/Components/Skills/Molecule/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Title = () => {
     return (
         <div className='text-primary text-center'>
-            <p className='text-4xl my-5'>My Skills</p>
+            <p className='text-4xl my-5'>My <span className='text-secondary'>Skills</span></p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut voluptatum cum mollitia voluptas illum explicabo, unde quasi. Error earum repellendus quasi architecto ipsum dignissimos similique tenetur itaque et! Esse, pariatur!</p>
         </div>
     )
