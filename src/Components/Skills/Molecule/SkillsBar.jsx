13d2315aac2226d53e83169dfbbabfbb538bb11f
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-11 13:57:29 +0800

    change: md margin

--- src/Components/Skills/Molecule/SkillsBar.jsx
+++ src/Components/Skills/Molecule/SkillsBar.jsx
@@ -5,7 +5,7 @@ export const SkillsBar = ({ data }) => {
         <div className='md:grid md:grid-cols-2 md:gap-9 mx-10'>
             {data.map((res, key) => {
                 return (
-                    <div key={key} className='my-10 md:my-4 text-primary'>
+                    <div key={key} className='my-10 md:my-2 text-primary'>
                         <p>{res.name}</p>
                         <div className="w-full bg-gray-200 rounded-full">
                             <div className="bg-blue-700 text-xs font-medium text-primary text-center p-0.5 leading-none rounded-l-full" style={{ width: `${res.barProgres}` }}>{res.barProgres}</div>
