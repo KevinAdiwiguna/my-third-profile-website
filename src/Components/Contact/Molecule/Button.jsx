c0fa596f254fbede462c1db890ccc851152eea7e
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-11 11:20:12 +0800

    add: margin y

--- src/Components/Contact/Molecule/Button.jsx
+++ src/Components/Contact/Molecule/Button.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Button = () => {
   return (
-    <button type="submit" className='bg-primary p-3 px-5 rounded-full hover:bg-secondary hover:text-background outline-none border-none duration-300'>Submit</button>
+    <button type="submit" className='bg-primary p-3 px-5 rounded-full hover:bg-secondary hover:text-background outline-none border-none duration-300 my-5'>Submit</button>
   )
 }
