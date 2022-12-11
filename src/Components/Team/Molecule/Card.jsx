97837c08d124e0234ddaa74dd4869a6a7811e6e5
kevin23566 <kevinadiwiguna.school@gmail.com>
2022-12-11 10:07:28 +0800

    add: shadow

--- src/Components/Team/Molecule/Card.jsx
+++ src/Components/Team/Molecule/Card.jsx
@@ -12,7 +12,7 @@ export const Card = ({ teams }) => {
         <div className='text-primary my-6 m-auto text-center grid justify-center items-center md:grid-cols-2 lg:grid-cols-3'>
             {teams.map((res, key) => {
                 return (
-                    <div key={key} className='bg-primary max-w-xs text-black m-5 rounded-xl'>
+                    <div key={key} className='bg-primary max-w-xs text-black m-5 rounded-xl shadow-md shadow-zinc-600'>
                         <img src={res.img} alt="" className='w-full rounded-xl' />
                         <div className='py-5 my-1'>
                             <p className='text-xl'>{res.name}</p>
